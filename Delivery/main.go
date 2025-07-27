5b49d1fa6d4d894047cd940a50878012406d2505
abnsol <abenisolo51196@gmail.com>
2025-07-27 23:33:19 +0300

    feat: add Delivery/main.go to initialize delivery layer

---
+++ Delivery/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"task_management/Delivery/config"
+	route "task_management/Delivery/routers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	config.LoadEnv()
+	database := config.Connect_db()
+	router := gin.Default()
+	route.Setup(*database, router)
+}
