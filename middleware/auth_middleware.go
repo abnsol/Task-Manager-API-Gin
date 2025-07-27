a8f786639096144d9a47d902707ffaf1e5e5b70b
abnsol <abenisolo51196@gmail.com>
2025-07-27 08:25:33 +0300

    feat(auth): add authentication middleware
    - Implement authentication middleware in middleware/auth_middleware.go

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,80 @@
+package middleware
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		jwtSecret := []byte(os.Getenv("jwt_secret"))
+		// JWT validation logic
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(401, gin.H{"error": "Authorization header is required"})
+			c.Abort()
+			return
+		}
+
+		authParts := strings.Split(authHeader, " ")
+		if len(authParts) != 2 || strings.ToLower(authParts[0]) != "bearer" {
+			c.JSON(401, gin.H{"error": "Invalid authorization header"})
+			c.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(authParts[1], func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(401, gin.H{"error": "Invalid JWT"})
+			c.Abort()
+			return
+		}
+
+		// Set claims in context for RoleMiddleware
+		if claims, ok := token.Claims.(jwt.MapClaims); ok {
+			c.Set("claims", claims)
+		}
+
+		c.Next()
+	}
+}
+
+// checks if the user has the required role
+func RoleMiddleware(requiredRole string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		claimsVal, exists := c.Get("claims")
+		if !exists {
+			c.JSON(401, gin.H{"error": "JWT claims not found"})
+			c.Abort()
+			return
+		}
+
+		claims, ok := claimsVal.(jwt.MapClaims)
+		if !ok {
+			c.JSON(401, gin.H{"error": "Invalid JWT claims"})
+			c.Abort()
+			return
+		}
+
+		role, ok := claims["role"].(string)
+		if !ok || role != requiredRole {
+			c.JSON(403, gin.H{"error": "Forbidden: Unauthorized"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
