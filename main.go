fca7f34deed55232b8956042698c77752de2ad38
abnsol <abenisolo51196@gmail.com>
2025-07-27 08:29:30 +0300

    feat: update main.go with authentication branch changes

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "task_management/router"
+import (
+	"log"
+	"task_management/data"
+	"task_management/router"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	// Load env
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	data.InitDB()
 	router.Routes()
+
 }
