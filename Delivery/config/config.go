72f015dbdd19174f7de7e7d95c829a553e29aad5
abnsol <abenisolo51196@gmail.com>
2025-07-27 23:27:57 +0300

    feat(config): add initial config file for Delivery layer

---
+++ Delivery/config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Connect_db() *mongo.Database {
+	mongoURI := os.Getenv("mongoURI")
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := client.Database("Tasks-DataBase")
+	return db
+}
+
+func LoadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
